cf21e6740134b767404345482e6f31a95139826e
Heng Sheng Yao <88011975+Sheng-Yao@users.noreply.github.com>
2024-08-22 13:09:04 +0800

    Update Temp_Humi_Function.h

--- src/Temp_Humi_Function.h
+++ src/Temp_Humi_Function.h
@@ -45,14 +45,12 @@ void getDHT22Info(enum Types type){
 
 // Temperature and Humidity data can only be obtained every 2 seconds for DHT22 while 1 second for DHT11
 
-float getTemperature(){
-    sensors_event_t event;
+float getTemperature(sensors_event_t event){
     dht.temperature().getEvent(&event);
     return event.temperature;
 }
 
-float getHumidity(){
-    sensors_event_t event;
+float getHumidity(sensors_event_t event){
     dht.humidity().getEvent(&event);
     return event.relative_humidity;
 }
